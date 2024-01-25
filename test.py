99a8514563132dbd71476befb55e7c3230383146
rinsay <rinsay.com@gmail.com>
2024-01-25 22:59:41 +0800

    test

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 from selenium import *
 
 
 url = "https://bilibili.com"
-headers = {}
-bili = requests.get(url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+bili = requests.get(url, headers=headers)
 cookies = bili.cookies
 for cookie in cookies:
     print(f"Name: {cookie.name}, Value: {cookie.value}")
